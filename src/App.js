6fc2514ee585614b6b51e9b964174af0dc43b05f
joshimayank312 <joshimayankh312@gmail.com>
2024-04-12 16:26:24 +0530

    first upload

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import AddUser from "./components/Users/AddUser";
+
+const App = () => {
+  return (
+    <>
+      <AddUser />
+    </>
+  );
+};
+
+export default App;
